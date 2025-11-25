caa87dcc2b8b3d48d6ef4ea6ff43615564a21d64
Wil Front <wilfront@outlook.com>
2025-11-25 10:27:13 -0300

    header arrumada

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
-import { CartProvider } from '@/context/CartContext';
+import { CartProvider } from "@/context/CartContext";
+import Header from "@/components/Header"; // ajuste o caminho conforme onde está seu Header.jsx
 import "./globals.css";
 
 export const metadata = {
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="pt-BR">
       <body>
         <CartProvider>
+          <Header />   {/* Header aparece em todas as páginas */}
           {children}
         </CartProvider>
       </body>
